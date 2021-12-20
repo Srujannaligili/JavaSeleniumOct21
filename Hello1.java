05c9a73ae627b5fa76b07b2455b2c21999814a13
Srujan Kumar <srujan.naligili@gmail.com>
2021-12-20 08:46:16 +0530

    Secong person check-in after changed

---
+++ Hello1.java
@@ -0,0 +1,12 @@
+public class Hello{
+
+public static void main(String[] args){
+
+System.out.print("Hello world!!!");
+System.out.print("Welcome world!!!");
+System.out.print("Hello world!!!");
+System.out.print("Welcome world!!!");
+}
+
+
+}
